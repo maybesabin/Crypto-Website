ccf1d894f1866c9472b0a428a7aebbbb89c7b743
maybesabin <highsabin987@gmail.com>
2024-10-07 08:24:06 +0545

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             {/* Navbar  */}
             <div className="lg:w-[75vw] w-full lg:px-0 px-4 z-[1000] bg-black">
                 <div className="flex items-center justify-between pt-9 opacity-100 relative">
-                    <h1 className="main-font text-[#bae626] font-semibold text-3xl">CRYPTO.</h1>
+                    <h1 className="main-font text-[#bae626] font-semibold md:text-3xl text-xl">CRYPTO.</h1>
 
                     <ul className="lg:flex hidden items-center justify-center gap-9 text-[1rem] text-zinc-400">
                         <li className="hover:text-white transition-all"><a href="#">Home</a></li>
@@ -30,7 +30,7 @@ const Navbar = () => {
                     >
                         <svg
                             aria-label="Main Menu"
-                            width="54" height="54" viewBox="0 0 100 100">
+                            width="34" height="34" viewBox="0 0 100 100">
                             <path
                                 className="line line1"
                                 d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
@@ -46,14 +46,14 @@ const Navbar = () => {
             </div>
 
             {/* Mobile Navbar  */}
-            <div className={`h-screen lg:hidden py-[9rem] transition-all duration-500 ease-in-out${showNavbar ? "flex translate-x-0 opacity-100" : "hidden translate-x-[40rem] opacity-75"} flex-col items-start justify-start fixed bg-black text-white z-[999] w-screen px-6`}>
-                <ul className="flex flex-col items-start justify-center h-full gap-[4rem] text-5xl text-zinc-400">
+            <div className={`h-screen lg:hidden py-[9rem] transition-all duration-500 ease-in-out${showNavbar ? "flex translate-x-0 opacity-100" : "hidden translate-x-[40rem] opacity-75"} flex-col items-start justify-center fixed bg-black text-white z-[999] w-screen px-6`}>
+                <ul className="flex flex-col items-start justify-center h-full gap-12 text-3xl text-zinc-400">
                     <li className="hover:text-white transition-all"><a href="#" className="main-font">Home</a></li>
                     <li className="hover:text-white transition-all"><a href="#" className="main-font">Services</a></li>
                     <li className="hover:text-white transition-all"><a href="#" className="main-font">About</a></li>
                     <li className="hover:text-white transition-all"><a href="#" className="main-font">What's New?</a></li>
                 </ul>
-                <button className="bg-[#bbfd03] hover:bg-[#ceff46] items-center justify-center transition-all text-black font-semibold text-xl h-16 w-auto px-7 rounded-xl">
+                <button className="bg-[#bbfd03] main-font hover:bg-[#ceff46] items-center justify-center transition-all text-black font-semibold text-[1rem] h-14 w-auto px-6 -mt-[5rem] rounded-xl">
                     Explore Now
                 </button>
             </div>
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,10 +7,10 @@ const Portfolio = () => {
             {/* Grid background  */}
             <div className="absolute pointer-events-none inset-0 flex items-start justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-            <section className='lg:w-[75vw] lg:px-0 px-9 h-auto py-[7rem] flex items-start justify-between relative pt-[7rem]'>
-                <div className='flex flex-col items-center justify-start w-full gap-4 text-7xl font-bold tracking-wider'>
+            <section className='lg:w-[75vw] w-full lg:px-0 px-4 flex items-center justify-between relative py-[7rem]'>
+                <div className='flex flex-col items-center justify-center w-full gap-4 lg:text-7xl text-4xl text-center font-bold tracking-wider'>
 
-                    <h1 className='bg-gradient-to-r from-white to-gray-700 bg-clip-text text-transparent main-font'>
+                    <h1 className='bg-gradient-to-r flex gap-3 flex-wrap text-center from-white to-gray-700 bg-clip-text text-transparent main-font'>
                         Build your <span className="main-font text-[#bcfd04]">crypto</span> portfolio.
                     </h1>
                 </div>
